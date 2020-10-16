c8f9ed6a9f4daa39c795538a8e4a762463e7476f
Sergey <example@gmail.com>
2020-10-16 15:58:12 +0400

    Bubble test

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ for (var i = 0; i < scores.length; i++) {
   output = "Bubble solution #" + i + " score: " + scores[i];
   console.log(output);
 }
+
+
+var highScore = 0;
+var output;
+for (var i = 0; i < scores.length; i++) {
+  output = "Bubble solution #" + i + " score: " + scores[i];
+  console.log(output);
+
+  if (scores[i] > highScore) {
+    highScore = scores[i];
+  }
+}
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+  if (scores[i] == highScore) {
+    bestSolutions.push(i);
+  }
+}
+console.log("Solutions with the highest score: " + bestSolutions);
